9744972376adb48f945de9fbee2af5463b89d528
01xhero <ospinoaddel@gmail.com>
2025-04-28 19:38:56 -0500

    Segundo commit

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,21 @@
-const wrappper = document.querySelector('.wrapper');
-const registerLink = document.querySelector('.register-link');
+const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
-registerLink.onclick = () => {
-    wrappper.classList.add('active');
-}
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+    });
 
-loginLink.onclick = () => {
-    wrappper.classList.remove('active');
-}
+loginLink.addEventListener('click', ()=> {
+     wrapper.classList.remove('active');
+     });
+
+btnPopup.addEventListener('click', () => {
+        wrapper.classList.add('active-popup');
+        });
+
+iconClose.addEventListener('click', () => {
+         wrapper.classList.remove('active-popup');
+        });
